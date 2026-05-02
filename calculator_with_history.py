01ef85c848ce2b5e9c2b8ef8599ac3fd7e0a74e5
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-05-02 18:40:20 +0800

    this is wehre the UI or the widget is getting built. this is where we put the label in the window. this is also wehre we store the operations that is going to be used in the calculator app

--- calculator_with_history.py
+++ calculator_with_history.py
@@ -6,3 +6,11 @@ class CalculatorApp:
         self.root = root
         self.root.title("Simple App Calculator")
         self.root.geometry("300x400")
+
+        self.label = tk.Label(root, text="Select Operation:", font=("Arial", 10, "bold"))
+        self.label.pack(pady=10)
+
+        self.operation = tk.StringVar(value="Addition")
+        operations = ["Addition", "Subtraction", "Multiplication", "Division"]
+        self.dropdown = tk.OptionMenu(root, self.operation, *operations)
+        self.dropdown.pack(pady=5)
