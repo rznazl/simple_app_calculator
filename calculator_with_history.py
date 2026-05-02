e72e1c50fdd22a7830b2906e005bab9ff80de55f
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-05-02 18:32:12 +0800

    this is where we put everything in a class to keep the code organized. the root part is wehre all the buttons and text will be stored

--- calculator_with_history.py
+++ calculator_with_history.py
@@ -1,3 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 
+class CalculatorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Simple App Calculator")
+        self.root.geometry("300x400")
